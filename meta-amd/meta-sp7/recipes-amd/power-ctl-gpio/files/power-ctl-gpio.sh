bb5127f986825b3e6c1049ca42b18050ab0dfb98
Jayanth Othayoth <Jayanth.Othayoth@amd.com>
2025-04-16 01:11:58 -0500

    Revert "meta-amd:sp7:power-ctl-gpio: check for LTPI Link"
    This reverts commit 3d1de36fc1108f61bcee2e51d0cab211264b2d71.
    
    Signed-off-by: Jayanth Othayoth <Jayanth.Othayoth@amd.com>
    Change-Id: I91ed3fa5278458138e64ce0af4eb022e7003e034

--- meta-amd/meta-sp7/recipes-amd/power-ctl-gpio/files/power-ctl-gpio.sh
+++ meta-amd/meta-sp7/recipes-amd/power-ctl-gpio/files/power-ctl-gpio.sh
@@ -9,8 +9,6 @@ P0_MGMT_UPDATE_FLASH_0=1037
 P0_MGMT_UPDATE_FLASH_1=1039
 P0_MGMT_UPDATE_FLASH_2=1041
 
-LTPI_STATUS_ADDR=0x14c34108
-
 set_gpio_pwer_pins() {
     GPIO=$1
     value=$2
@@ -24,26 +22,6 @@ set_gpio_pwer_pins() {
     echo "$GPIO" > /sys/class/gpio/unexport
     return 0
 }
-
-value=$(devmem $LTPI_STATUS_ADDR)
-bit_field=$((value & 0xF))
-
-if [ "$bit_field" -ne 7 ]; then
-    if [ -f /var/lib/ltpi_reboot ]; then
-        echo "LTPI is NOT up even after reboot, Please AC CYCLE"
-        rm /var/lib/ltpi_reboot
-    else
-        touch /var/lib/ltpi_reboot
-        echo "Rebooting system due to LTPI not being up"
-        reboot
-    fi
-    exit 1
-else
-    if [ -f /var/lib/ltpi_reboot ]; then
-        rm /var/lib/ltpi_reboot
-    fi
-    echo  "LTPI Link is up"
-fi
 set_gpio_pwer_pins "$P0_MGMT_ASSERT_PROCHOT_L" "0"
 set_gpio_pwer_pins "$P0_MGMT_ASSERT_CLR_CMOS" "0"
 set_gpio_pwer_pins "$P0_ASSERT_RSMRST" "0"
