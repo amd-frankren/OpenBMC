7297775e26c9222b15af6500902b15111d7f41bd
modolaty <mohsen.dolaty@amd.com>
2025-04-17 09:48:12 -0500

    meta-amd:common:apml: add support for second SBTSI
    Modify set-apml.sh
    - add second SBTSI device for i3c-4 and i3c-5
    - detect Num of CPU based on board ID
    
    Tested:
    - verified in Congo RevA with fused IOD
    
    Signed-off-by: modolaty <mohsen.dolaty@amd.com>
    Change-Id: I5256ebeadd6bf024cf4f08dda41f1025fdabe56c

--- meta-amd/meta-common/recipes-amd/amd-apml/files/set-apml.sh
+++ meta-amd/meta-common/recipes-amd/amd-apml/files/set-apml.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-num_of_cpu=$(/sbin/fw_printenv -n num_of_cpu)
+board_id=$(/sbin/fw_printenv -n board_id)
+case "$board_id" in
+    # Congo(0x80, 0x81, 0x86)
+    # Kenya(0x84)
+    # Senegal (0x88)
+    # Sahara (0x89)
+    # Zambia (0x8B)
+    # Zimbabwe (0x8C)
+    # Zanzibar (0x8D)
+    "80" | "81" | "84" | "86" | "88" | "89" | "8B" | "8C" | "8D")
+        num_of_cpu=1
+        ;;
+    # Morocco(0x82, 0x83, 0x87)
+    # Malawi (0x8A)
+    # Nigeria(0x85)
+    # Ghana (0x8E)
+    "82" | "83" | "85" | "87" | "8A" | "8E")
+        num_of_cpu=2
+        ;;
+    *)
+    # all i3c devices must be unbind before removing i3c driver
+    # it is safer to remove devices for both P0/P1 if Board ID is unknown
+        num_of_cpu=2
+        ;;
+esac
 
 DEV_I3C_SBTSI_PATH="/sys/bus/i3c/drivers/sbtsi_i3c"
 DEV_I3C_SBTSI_4="4-118"
+DEV_I3C_SBTSI_4_1="4-10118"
 DEV_I3C_SBTSI_5="5-118"
+DEV_I3C_SBTSI_5_1="5-10118"
 
 DEV_I3C_SBRMI_PATH="/sys/bus/i3c/drivers/sbrmi_i3c"
 DEV_I3C_SBRMI_4="4-1118"
@@ -25,10 +51,16 @@ unbind_i3c_drivers()
     # Unbind sbtsi and sbrmi drivers
     echo  $DEV_I3C_SBTSI_4 > $DEV_I3C_SBTSI_PATH/unbind
     sleep 1
+    echo  $DEV_I3C_SBTSI_4_1 > $DEV_I3C_SBTSI_PATH/unbind
+    sleep 1
     echo  $DEV_I3C_SBRMI_4 > $DEV_I3C_SBRMI_PATH/unbind
+    sleep 1
     if [[ $num_of_cpu == 2 ]]
     then
         echo  $DEV_I3C_SBTSI_5 > $DEV_I3C_SBTSI_PATH/unbind
+        sleep 1
+        echo  $DEV_I3C_SBTSI_5_1 > $DEV_I3C_SBTSI_PATH/unbind
+        sleep 1
         echo  $DEV_I3C_SBRMI_5 > $DEV_I3C_SBRMI_PATH/unbind
     fi
     # Remove platform i3c driver
